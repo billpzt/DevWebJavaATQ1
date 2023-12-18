638c7ced8fff926814060096af270a9a0435bfad
Billpzt <billpiazzetta@gmail.com>
2023-12-18 00:46:15 -0300

    simplificar métodos dando apenas return

--- src/main/java/org/example/Complexo.java
+++ src/main/java/org/example/Complexo.java
@@ -5,17 +5,14 @@ public class Complexo {
     }
 
     public static double soma(double n1, double n2){
-        double resultado = n1 + n2;
-        return resultado;
+        return n1 + n2;
     }
 
     public static double sub(double n1, double n2){
-        double resultado = n1 - n2;
-        return resultado;
+        return n1 - n2;
     }
 
     public static double modulus(double n1, double n2){
-        double  resultado = Math.pow(n1,2) + Math.pow(n2,2);
-        return resultado;
+        return Math.pow(n1,2) + Math.pow(n2,2);
     }
 }
